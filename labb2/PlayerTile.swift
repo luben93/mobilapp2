982c10a1e643bfcf066659343b6ba73c863b8356
sirprice <carljohan.dahlman@gmail.com>
2016-11-22 14:42:20 +0100

    PlayerTile testing lol

---
+++ labb2/PlayerTile.swift
@@ -0,0 +1,21 @@
+//
+//  PlayerTile.swift
+//  labb2
+//
+//  Created by Carl-Johan Dahlman on 2016-11-22.
+//  Copyright © 2016 lucas persson. All rights reserved.
+//
+
+import UIKit
+
+class PlayerTile: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
