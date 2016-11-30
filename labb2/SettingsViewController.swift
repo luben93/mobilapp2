c464016ddbbe38ab2a8da8792569a21adf7709a4
sirprice <carljohan.dahlman@gmail.com>
2016-11-30 22:18:43 +0100

    message

---
+++ labb2/SettingsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsViewController.swift
+//  labb2
+//
+//  Created by Carl-Johan Dahlman on 2016-11-30.
+//  Copyright © 2016 lucas persson. All rights reserved.
+//
+
+import UIKit
+class SettingsViewController: UIViewController {
+    
+    
+    
+}
