fa11aff7c0901ee39157748d3b70a326e6349b6f
sirprice <carljohan.dahlman@gmail.com>
2016-12-01 00:45:02 +0100

    adsfasdf

--- labb2/SettingsViewController.swift
+++ labb2/SettingsViewController.swift
@@ -9,8 +9,40 @@
 import UIKit
 class SettingsViewController:  UIViewController
 {
-   
+    @IBOutlet weak var lineColorButton: UIButton!
+    @IBOutlet weak var lineColorBox: UIView!
+    @IBOutlet weak var backColorButton: UIButton!
+    
+    @IBOutlet weak var backColorBox: UIView!
+    @IBOutlet weak var theView: UIView!
+    
+    var selectedButton:UIButton = UIButton()
     
     
+    let colorslider = ColorSlider()
     
+    override func viewDidLoad() {
+        colorslider.frame = CGRect(x: 0, y: 0, width: 40, height: 350)
+        //view.addSubview(colorslider)
+        theView.addSubview(colorslider)
+        colorslider.addTarget(self, action: #selector(SettingsViewController.changedColor(_:)), for: .valueChanged)
+        
+        
+        
+        
+    }
+    
+    
+    func changedColor(_ slider: ColorSlider) {
+        var color = slider.color
+        print("Color: \(color.description)")
+    }
+    
+    @IBAction func lineColorSelected(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func backColorSelected(_ sender: UIButton) {
+        
+    }
 }
